3257a75421b1932e3a01818757b8b0e72bfe05fb
LIICHUAN <annachin0424@gmail.com>
2026-06-07 19:32:00 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,14 +1,13 @@
 import os
 import discord
-from keep_alive import keep_alive
 from discord.ext import commands
+from keep_alive import keep_alive
 
 intents = discord.Intents.default()
-intents.message_content = True
-intents.voice_states = True
+intents.message_content = True   
+intents.voice_states = True     
 
 bot = commands.Bot(command_prefix='!', intents=intents)
-
 @bot.event
 async def on_ready():
     print(f'✅ 机器人已上线：{bot.user.name}')
